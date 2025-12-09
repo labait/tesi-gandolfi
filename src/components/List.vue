440ebf145d7c310af34c9551f8f1a0e7400d3751
Antonio Molinari <antoniomolinari@me.com>
2025-12-09 16:22:38 +0100

    Refactor App.vue to integrate Nav component for authentication and navigation. Update List component to display item titles over images. Add new Nav component for handling user authentication and profile navigation.

--- src/components/List.vue
+++ src/components/List.vue
@@ -14,8 +14,11 @@ const props = defineProps({
 
   <div 
     class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-    <img 
-      v-for="item in items" :src="item.image" :alt="item.alt" 
-      class="cursor-pointer grayscale w-full h-full object-cover rounded-lg hover:grayscale-0 hover:scale-120 hover:shadow-lg hover:rotate-1 hover:z-10 transition-all duration-300" />
+    <div v-for="item in items" :key="item.image" class=" hover:scale-120 hover:shadow-lg hover:rotate-1 hover:z-10 transition-all duration-300">
+      <div v-if="item.title" class="title absolute bottom-0 left-0 right-0 bg-black/50 text-white p-2 rounded-b-lg z-10">{{ item.title }}</div>
+      <img :src="item.image" :alt="item.alt" 
+        class="cursor-pointer grayscale w-full h-full object-cover rounded-lg hover:grayscale-0" 
+      />
+    </div>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Homepage from '../views/Homepage.vue'
 import Search from '../views/Search.vue'
 import Profile from '../views/Profile.vue'
-
 const router = createRouter({
   history: createWebHistory(),
   routes: [
